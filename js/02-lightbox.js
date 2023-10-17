5deca9a2bbba048a16314747fef821975e46569a
Kateryna Tkachenko <Tkachenko.city@gmail.com>
2023-10-17 20:35:54 +0300

    start task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import SimpleLightbox from "simplelightbox/dist/simple-lightbox.esm";
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+// Change code below this line
+const refs = {
+  listGallery: document.querySelector(".gallery"),
+  //   body: document.querySelector("body"),
+};
+
+// refs.listGallery.addEventListener("click", selectImg);
+
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) => `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" title="${description}"/>
+        </a>
+        </li>`
+    )
+    .join("");
+}
+
+refs.listGallery.innerHTML = createMarkup(galleryItems);
+
+console.log(SimpleLightbox);
