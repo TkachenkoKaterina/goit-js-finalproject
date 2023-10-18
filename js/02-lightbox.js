1c2dbedc8c477394125c392ac00981205a99c88b
Kateryna Tkachenko <Tkachenko.city@gmail.com>
2023-10-18 22:53:36 +0300

    add task 2+

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,13 +1,9 @@
-import SimpleLightbox from "simplelightbox/dist/simple-lightbox.esm";
 import { galleryItems } from "./gallery-items.js";
 
 // Change code below this line
 const refs = {
   listGallery: document.querySelector(".gallery"),
-  //   body: document.querySelector("body"),
 };
-
-// refs.listGallery.addEventListener("click", selectImg);
 
 function createMarkup(arr) {
   return arr
@@ -15,7 +11,7 @@ function createMarkup(arr) {
       ({ preview, original, description }) => `
       <li class="gallery__item">
         <a class="gallery__link" href="${original}">
-            <img class="gallery__image" src="${preview}" alt="${description}" title="${description}"/>
+            <img class="gallery__image" src="${preview}" alt="${description}"/>
         </a>
         </li>`
     )
@@ -24,4 +20,7 @@ function createMarkup(arr) {
 
 refs.listGallery.innerHTML = createMarkup(galleryItems);
 
-console.log(SimpleLightbox);
+let gallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
